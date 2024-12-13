5bbee5b72fc2d67ca6cceb070d57c848076bd090
brikeld <129516277+brikeld@users.noreply.github.com>
2024-12-13 20:44:59 +0100

    1
    1

---
+++ Web Funziona 2/script.js
@@ -0,0 +1,38 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const svgObject = document.getElementById("labyrinth-svg");
+    svgObject.addEventListener("load", function() {
+        const svgDoc = svgObject.contentDocument;
+        const textPath = svgDoc.getElementById("labyrinth-text");
+        const textPathReverse = svgDoc.getElementById("labyrinth-text-reverse");
+        const textPath2 = svgDoc.getElementById("labyrinth-text-2");
+        const textPath3 = svgDoc.getElementById("labyrinth-text-3");
+
+        gsap.to(textPath, {
+            duration: 100, // Increase the duration to decrease the speed
+            attr: { startOffset: "100%" },
+            repeat: -1,
+            ease: "linear"
+        });
+
+        gsap.to(textPathReverse, {
+            duration: 200, // Increase the duration to decrease the speed
+            attr: { startOffset: "0%" },
+            repeat: -1,
+            ease: "linear"
+        });
+
+        gsap.to(textPath2, {
+            duration: 500, // Increase the duration to decrease the speed
+            attr: { startOffset: "100%" },
+            repeat: -1,
+            ease: "linear"
+        });
+
+        gsap.to(textPath3, {
+            duration: 500, // Increase the duration to decrease the speed
+            attr: { startOffset: "0%" },
+            repeat: -1,
+            ease: "linear"
+        });
+    });
+});
