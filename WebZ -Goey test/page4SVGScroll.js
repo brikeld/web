218f47e7e0b37b44c1fce6179d4e6ade4e39ab2b
brikeld <129516277+brikeld@users.noreply.github.com>
2025-02-10 16:21:47 +0100

    modification
    mod

--- WebZ -Goey test/page4SVGScroll.js
+++ WebZ -Goey test/page4SVGScroll.js
@@ -15,6 +15,7 @@ function initPage4SVGScroll() {
         
         canvas.width = img.width;
         canvas.height = img.height;
+        /////
         
         ctx.drawImage(img, 0, 0);
         const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
